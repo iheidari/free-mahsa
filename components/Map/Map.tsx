34b2bf37a67a28857953983101df13d05764fd8f
Iman Heidari <iman.heydari@gmail.com>
2022-11-06 00:36:19 -0700

    fix center positions for provinces

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -6,26 +6,27 @@ type Props = {};
 
 const Map = (props: Props) => {
   return (
-    <svg
-      //   baseprofile="tiny"
-      fill="#7c7c7c"
-      stroke="#ffffff"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      version="1.2"
-      viewBox="0 0 1000 908"
-      onMouseUp={(event) => console.dir(event)}
-    >
-      {paths.map((path) => (
-        <React.Fragment key={path.id}>
-          <path d={path.d} fill="lightblue"></path>
-          <text x={path.x} y={path.y}>
-            {path.id}
-          </text>
-        </React.Fragment>
-      ))}
-    </svg>
+    <div style={{ width: "700px", backgroundColor: "gray" }}>
+      <svg
+        fill="#7c7c7c"
+        stroke="#ffffff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        version="1.2"
+        viewBox="0 0 1000 908"
+        onMouseUp={(event) => console.dir(event)}
+      >
+        {paths.map((path) => (
+          <React.Fragment key={path.id}>
+            <path d={path.d} className="hover:fill-slate-500"></path>
+            <text x={path.x} y={path.y}>
+              {path.id}
+            </text>
+          </React.Fragment>
+        ))}
+      </svg>
+    </div>
   );
 };
 
